a2eea7c71613ca80f8ed224e2321d9ecb76319de
torii katsuya <torikatsu@toriinoMacBook-Pro.local>
2021-03-04 01:42:54 +0900

    add comment

--- installer.sh
+++ installer.sh
@@ -3,4 +3,5 @@ script_path=~/dotfiles
 sh $script_path"/tmux/installer.tmux.sh"
 sh $script_path"/vim/installer.vim.sh"
 sh $script_path"/zsh/installer.zsh.sh"
+# installer.brew.sh must be excecuted after installer.zsh.sh
 sh $script_path"/homebrew/installer.brew.sh"
